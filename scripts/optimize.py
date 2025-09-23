0f440d8928525598e34e9e36751215ae8a1725e4
l-baxter1 <l-baxter@onu.edu>
2025-09-22 23:22:36 -0400

    more work on optimize

--- scripts/optimize.py
+++ scripts/optimize.py
@@ -1,13 +1,18 @@
 
 # this is where the actual optimization will happen
+# maybe make a copy of this and remove annotations when done. 
+#   put the annotated version in git_ignore for reference
 
 
 import string
+import numpy as np
 from powerplant import PowerPlant
 from excel_read import get_conditions, get_plants  # DELETE THIS WHEN DONE TESTING THIS FILE
-from pymoo.optimize import minimize
-from pymoo.core.problem import ElementwiseProblem
+
 from pymoo.algorithms.soo.nonconvex.ga import GA as gen_alg 
+from pymoo.core.population import Population
+from pymoo.core.problem import ElementwiseProblem
+from pymoo.optimize import minimize
 from pymoo.termination import get_termination
 
 
@@ -28,47 +33,61 @@ def optimize(opt_type: string, plants: list[PowerPlant], conditions: list[float]
 def opt_cost(plants: list[PowerPlant], conditions: list[float]):
 
     # easy access to important variables!!
-    population_size = 1000  # define population size
-    num_gens = 100  # number of generations to run
-    number_of_bits = 200  # this is the number of bits in a bitstring. we're trying to make them all 1, so this is also the optimal sum. cool :thumbsup:
-    bitstring_line_break = 50  # number of bits shown per line in the output. improves readability. bigger numbers are better for bigger screens.
+    population_size = 100  # will be used later in _evaluate for finding "x" array dimensions
+    # TODO what exactly is this? wrap your little baby brain around it
+    num_gens = 10  # number of generations to run. small for testing :P
 
+    # this will be a list of costs..? for now at least........???????
+    # or maybe a numpy array where only the cost is accessed? is that possible
+    initial_population = Population().new()  # TODO how do i initialize a population? like how do i add stuff. theres no append
+    for plant in plants:
+        # initial_population.new()  # WHY IS "CUMSUM" AN OPTION HERE. WHO THE FUCK MADE THAT.
+        pass  # so the file name isn't red and evil
+    
     class CostOptProblem(ElementwiseProblem):
         def __init__(self):
+
+            # n_var is number of decision (design?) vars; for now is just amount of power from each plant
+            #   - it's what's changed each run (each plant's energy output, so must = number of plants)
+            #   - also number of columns in input
+            # n_obj is number of objectives; WILL STAY 1 BECAUSE SINGLE OBJECTIVE, objective is cost (minimize)
+            # xl and xu (upper and lower bounds for x) will be defined here, eventually. not now though.
             super().__init__(n_var=len(plants), n_obj=1)
 
         # tells the problem whats a good/feasible solution
         def _evaluate(self, x, out):
-            out["F"] = x.sum()  # f is objective function; this should sum costs specifically
-            # decision var is amt of power, funct is cost x megawatt
-            out["G"] = None  # TODO array of constraints
-
-    # defining the problem here so that I can use it in the sampling. 
-    prb = CostOptProblem()
+            # x definition from docs: 
+            # "x is a one-dimensional array of length [n_var] and is called [pop_size] times in each iteration"
+            #   - so here it's [100x1] ([16x1]?)
+            #   - numpy array !!
+            #   - trying to really figure out where x comes from. provided by initial population? my brain hurts
+            # f is objective function; this should calculate cost x megawatt.
+            out["F"] = x.sum()  # will be something like (x (cost) * decision var (mW)).sum()
+            # out["G"] =   # TODO array of constraints---just get it working first
 
     alg = gen_alg(
         pop_size=population_size,
+        sampling=initial_population,  # correct to have sampling here?
         eliminate_duplicates=True
     )
 
-    # terminates after a set number of generations. defined towards top.
-    trm = get_termination("n_gen", num_gens)
-
     result = minimize(
-        problem=prb,
-        algorithm=alg,
-        termination= trm,
+        problem=CostOptProblem(),
+        algorithm=alg,  # defined directly above this
+        termination=get_termination("n_gen", num_gens),
         verbose=True,
         save_history=True
     )
 
+    # this stuff's all just printing. can ignore for now.
+
     # print best sum achieved
-    print(f'Best sum {-result.pop.get("F").min()}/{number_of_bits}')
+    print(f'Best cost {result.pop.get("F").min()}')
 
     # put entire run history into variable for easier access
     history = result.history
 
-    # print best bitstring per generation and its sum
+    # print statement from onemax
     for run in history:
         index = 0
         print(f'Run {run.n_gen}:\n'  # print generation number
@@ -76,11 +95,8 @@ def opt_cost(plants: list[PowerPlant], conditions: list[float]):
         for bit in run.opt[0].X:
             index = index + 1
             print(f'{int(bit)}', end='')
-            if index == bitstring_line_break: 
-                print('\n       ', end='')  # adds a line break after 50 bits because otherwise it wraps weird
-                index = 0
         print(f'\n'
-              f'       Run {run.n_gen} sum: {-run.pop.get("F").min()}/{number_of_bits}\n')  # show generation's best sum
+              f'       Run {run.n_gen} sum: {run.pop.get("F").min()}')  # show generation's best sum
 
 
 # TESTING AREA. STUFF BELOW HERE WILL BE DELETED EVENTUALLY.
