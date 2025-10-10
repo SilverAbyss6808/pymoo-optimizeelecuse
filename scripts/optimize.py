a60f361ec4093bd29c0d9303740ab7def7c16767
l-baxter1 <l-baxter@onu.edu>
2025-10-10 13:10:06 -0400

    movin to desktop

--- scripts/optimize.py
+++ scripts/optimize.py
@@ -17,7 +17,6 @@ from pymoo.termination import get_termination
 def optimize(opt_type: string, plants: list[PowerPlant], conditions: list[float]):
     try:  # can add more types of optimization later (carbon, water use, etc)
         if opt_type == 'cost':
-            # TODO MAKE THIS INTO A NUMPY ARRAY MAYBE ???
             opt_cost(plants, conditions)
         else:
             raise Exception(f'Type {opt_type} not recognized (or not yet implemented).')
@@ -35,9 +34,11 @@ def opt_cost(plants: list[PowerPlant], conditions: list[float]):
     # defining the progress bar up here so it's easier to find
     progress_bar = alive_bar(
         total=num_gens, 
-        title='Optimizing data. Have a fish while you wait:', 
-        bar='fish',
-        spinner='classic'
+        title='Optimizing data, please wait...',
+        theme='smooth'
+        # title='Optimizing data. Have a fish while you wait:', 
+        # bar='fish',
+        # spinner='classic'
     ) 
     
 
@@ -53,8 +54,8 @@ def opt_cost(plants: list[PowerPlant], conditions: list[float]):
                 xl.append(0)
                 xu.append(p.max_output)
 
-            # number of constraints = number of plants (17), demand met (1)
-            num_constr = len(plants) + 1
+            # number of constraints = number of plants (17), demand met (1), more than one not max or 0 (1)
+            num_constr = len(plants) + 1 + 1
             super().__init__(n_var=len(plants), n_obj=1, xl=np.array(xl), xu=np.array(xu), n_constr=num_constr)
 
 
@@ -62,25 +63,36 @@ def opt_cost(plants: list[PowerPlant], conditions: list[float]):
             # reset tracking lists to empty
             self.cost_per_plant = []
             constraint_violations: list[bool] = []
+            notok_counter = 0
 
             power_demand: float = conditions[0]  # 13600 mW right now
             plant_costs = [p.plant_cost for p in plants]
 
+            for i in x: i = round(i)
+            print(x)
+
             for ind, mw in enumerate(x):
                 p = plants[ind]
-                mi = p.min_output
+                mi = 0
                 ma = p.max_output
 
-                # constraint violations: you want these to be FALSE !!!
-                # check if mw generated is out of bounds but not zero
-                notok = (mw > ma or mw < mi) and mw != 0
+                # constraint violations pt 1: mw generated <= max
+                # you want these to be FALSE !!!
+                notok = mw < ma and mw != 0
+                if notok and notok_counter < 1: 
+                    notok_counter += 1
+                    notok = False
                 constraint_violations.append(notok)  # adds cv boolean to end of array (true is 1, false is 0)
 
-                # TODO make it so <= 1 (so no more than one) plants can be below max power.
-                # one can be below min but not 0 to allow for this
+                # TODO constraint violations pt 2: no more than one plant is not running at max or 0
+                # this SHOULD make it so plants dont split
 
                 self.cost_per_plant.append([p.name, round(mw), round(mw * 1000 * p.plant_cost, 2)])  # x1000 because mw
 
+            if notok_counter > 1: constraint_violations.append(notok_counter)
+            else: constraint_violations.append(0)
+            
+            # constraint violations pt 3: was the demand met?
             demand_met = power_demand - x.sum()
             if demand_met < 0: demand_met = 0  # sets to 0 if the demand was successfully met
             constraint_violations.append(demand_met)
@@ -106,7 +118,8 @@ def opt_cost(plants: list[PowerPlant], conditions: list[float]):
             termination=get_termination('n_gen', num_gens),
             verbose=False,
             callback=ProgressBarCallback(),
-            save_history=True
+            save_history=True,
+            return_least_infeasible=True
         )
 
     # print best solution achieved
@@ -114,10 +127,10 @@ def opt_cost(plants: list[PowerPlant], conditions: list[float]):
 
     plant_info = [(p.plant_cost, p.max_output) for p in plants]
     for i, p in enumerate(result.problem.cost_per_plant):
-        # print(f'{i+1}. {p[0]} ({plant_info[i][0]})\n'
-        #       f'    mW produced:    {p[1]} (max: {plant_info[i][1]})\n'
-        #       f'    Total price:    ${p[2]}')
-        print(f'{p[1]}/{plant_info[i][1]}   {p[0]}')
+        print(f'{i+1}. {p[0]} ({plant_info[i][0]})\n'
+              f'    mW produced:    {p[1]} (max: {plant_info[i][1]})\n'
+              f'    Total price:    ${p[2]}')
+        # print(f'{p[1]}/{plant_info[i][1]}   {p[0]}')
 
 
     # # put entire run history into variable for easier access
