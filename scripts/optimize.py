0b27c8ee676799215e6fdb267bc384c978bc651b
l-baxter1 <l-baxter@onu.edu>
2025-10-13 15:09:08 -0400

    super basic graph!!

--- scripts/optimize.py
+++ scripts/optimize.py
@@ -1,14 +1,16 @@
 
 import string
+import matplotlib.pyplot as plt
 import numpy as np
 
 from alive_progress import alive_bar
-from excel_read import get_conditions, get_plants  # DELETE THIS WHEN DONE TESTING THIS FILE
+from multiprocessing.pool import ThreadPool
 from powerplant import PowerPlant
 
 from pymoo.algorithms.soo.nonconvex.ga import GA as gen_alg 
 from pymoo.core.callback import Callback
 from pymoo.core.problem import ElementwiseProblem
+from pymoo.core.problem import StarmapParallelization
 from pymoo.operators.crossover.sbx import SBX
 from pymoo.operators.mutation.pm import PM
 from pymoo.operators.repair.rounding import RoundingRepair
@@ -16,12 +18,20 @@ from pymoo.operators.sampling.rnd import IntegerRandomSampling
 from pymoo.optimize import minimize
 from pymoo.termination import get_termination
 
+# DELETE THIS SECTION WHEN DONE TESTING THIS FILE
+from excel_read import get_conditions, get_plants
+from display import display_costopt_graph
+
+
+# TODO parallellize this motherfucker. its SO fuckin slow oh my GOD
+
+# TODO graph solution
 
 # only one visible to main to keep things neat
 def optimize(opt_type: string, plants: list[PowerPlant], conditions: list[float]):
     try:  # can add more types of optimization later (carbon, water use, etc)
         if opt_type == 'cost':
-            opt_cost(plants, conditions)
+            return opt_cost(plants, conditions)
         else:
             raise Exception(f'Type {opt_type} not recognized (or not yet implemented).')
     except Exception as e:
@@ -32,63 +42,55 @@ def optimize(opt_type: string, plants: list[PowerPlant], conditions: list[float]
 def opt_cost(plants: list[PowerPlant], conditions: list[float]):
 
     # easy access to important variables!!
-    population_size = 200  # will be used later in _evaluate for finding "x" array dimensions. default to 100
-    num_gens = 1000  # number of generations to run
+    # BOTH OF THESE ARE DIVIDED BY 10 RN BECAUSE OTHERWISE IT TAKES TOO LONG TO RUN FOR TESTING
+    population_size = 25 # will be used later in _evaluate for finding "x" array dimensions. default to 100
+    num_gens = 200  # number of generations to run
+    num_threads = 8  # number of threads to use to speed this bitch UP
 
     # defining the progress bar up here so it's easier to find
     progress_bar = alive_bar(
         total=num_gens, 
         title='Optimizing data, please wait...',
         theme='smooth'
-        # title='Optimizing data. Have a fish while you wait:', 
-        # bar='fish',
-        # spinner='classic'
     ) 
     
 
     # CLASSES !!!
     class CostOptProblem(ElementwiseProblem):
-        def __init__(self):
+        def __init__(self, **kwargs):
             # xl and xu (upper and lower bounds for x/cost) here
             xl: list[int] = []
             xu: list[int] = []
-            self.cost_per_plant: list[str, float, float] = []  # plant name, cost/w, power generated, total mw cost
 
             for p in plants:  # finding min and max possible outputs for plants, ALL X VALS WILL BE BETWEEN THESE
                 xl.append(0)
                 xu.append(p.max_output)
 
-            # number of constraints = number of plants (17), demand met (1), more than one not max or 0 (1)
+            # number of constraints = number of plants (17), demand met (1)
             num_constr = len(plants) + 1
-            super().__init__(n_var=len(plants), n_obj=1, xl=np.array(xl), xu=np.array(xu), n_constr=num_constr, vtype=int)
+            super().__init__(n_var=len(plants), n_obj=1, xl=np.array(xl), xu=np.array(xu), n_constr=num_constr, vtype=int, **kwargs)
 
 
         def _evaluate(self, x, out):
             # reset tracking lists to empty
-            self.cost_per_plant = []
             constraint_violations: list[bool] = []
 
-            power_demand: float = conditions[0]  # 13600 mW right now
+            power_demand = conditions[0]  # 13600 mW right now
             plant_costs = [p.plant_cost for p in plants]
             notok_counter = 0
 
             for ind, mw in enumerate(x):
-                p = plants[ind]
-                ma = p.max_output
+                ma = plants[ind].max_output
 
                 # constraint violations pt 1: mw generated <= max
-                # you want these to be FALSE !!!
                 # constraint violations pt 2: no more than one plant is not running at max or 0
-                # this SHOULD make it so plants dont split
                 # i think it works? it just somehow cant find a good answer in a full minute of running
                 notok = mw < ma and mw != 0
                 if notok and notok_counter < 1: 
-                    constraint_violations.append(False)
                     notok_counter += 1
+                    constraint_violations.append(0)
                 else: constraint_violations.append(notok)
 
-                self.cost_per_plant.append([p.name, round(mw), round(mw * 1000 * p.plant_cost, 2)])  # x1000 because mw
-            
             # constraint violations pt 3: was the demand met?
             demand_met = power_demand - x.sum()
             if demand_met < 0: demand_met = 0  # sets to 0 if the demand was successfully met
@@ -103,6 +105,8 @@ def opt_cost(plants: list[PowerPlant], conditions: list[float]):
             bar()
 
 
+    prb = CostOptProblem()
+
     alg = gen_alg(
         pop_size=population_size,
         sampling=IntegerRandomSampling(),
@@ -113,7 +117,7 @@ def opt_cost(plants: list[PowerPlant], conditions: list[float]):
 
     with progress_bar as bar:
         result = minimize(  # go go gadget cheap electric
-            problem=CostOptProblem(),
+            problem=prb,
             algorithm=alg,  # defined directly above this
             termination=get_termination('n_gen', num_gens),
             verbose=False,
@@ -125,24 +129,20 @@ def opt_cost(plants: list[PowerPlant], conditions: list[float]):
     # print best solution achieved
     print('Best solution found: \nX = %s\nF = %s\nG = %s' % (result.X, result.F, result.G))
 
-    plant_info = [(p.plant_cost, p.max_output) for p in plants]
-    for i, p in enumerate(result.problem.cost_per_plant):
-        print(f'{i+1}. {p[0]} ({plant_info[i][0]})\n'
-              f'    mW produced:    {p[1]} (max: {plant_info[i][1]})\n'
-              f'    Total price:    ${p[2]}')
-        # print(f'{p[1]}/{plant_info[i][1]}   {p[0]}')
-
-
-    # # put entire run history into variable for easier access
-    # history = result.history
-
-    # # print each gen's best cost found
-    # for run in history:
-    #     print(f'Run {run.n_gen} best cost: {run.pop.get("F").min()}')  # show generation's best sum
+    plant_info = [(p.name, p.plant_cost, p.max_output) for p in plants]
+    for i, x in enumerate(result.X):
+        # print(f'{i+1}. {plant_info[i][0]} ({plant_info[i][1]})\n'
+        #       f'    mW produced:    {x} (max: {plant_info[i][2]})\n'
+        #       f'    Total price:    ${round(x * 1000 * plant_info[i][1], 2)}')
+        print(f'{x}/{plant_info[i][2]}   {plant_info[i][1]}')
+    
+    # prb.pool.close()
+    return result.X
 
 
 # TESTING AREA. STUFF BELOW HERE WILL BE DELETED EVENTUALLY.
 plants = get_plants('git_ignore\\CE4321_GridOptimizer_v3.xlsx')
 conditions = get_conditions('git_ignore\\CE4321_GridOptimizer_v3.xlsx')
 
-optimize('cost', plants, conditions)
+optimal_costs = optimize('cost', plants, conditions)
+display_costopt_graph(optimal_costs, plants)
