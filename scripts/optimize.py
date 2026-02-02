598beb940100fae7b5003c06f9ccaadf48c70b7a
l-baxter1 <l-baxter@onu.edu>
2026-02-02 09:12:21 -0500

    some moo stuff

--- scripts/optimize.py
+++ scripts/optimize.py
@@ -30,8 +30,8 @@ def optimize(opt_type: string, plants: list[PowerPlant], conditions: list[float]
     pop_size = 250  # 250
     n_gens = 50  # 25
 
-    n_runs = 16
-    n_threads = 8
+    n_runs = 1
+    n_threads = 1
     n_batches = int(n_runs/n_threads)
 
     # progress_bar = alive_bar(
@@ -44,34 +44,38 @@ def optimize(opt_type: string, plants: list[PowerPlant], conditions: list[float]
     opt_start = time.time()
     print(f'Starting...')
 
-    match opt_type:
-        case 'cost':
-            run_count = 1
-            for b in range(n_batches):  # total number of times that each thread has to run in order to hit n_runs, if that makes sense
-                # with progress_bar as bar:
-                threads = []
-                time_start = time.time()
+    run_count = 1
+    for b in range(n_batches):  # total number of times that each thread has to run in order to hit n_runs, if that makes sense
+        # with progress_bar as bar:
+        threads = []
+        time_start = time.time()
 
-                for i in range(n_threads):
+        for i in range(n_threads):
+            match opt_type:
+                case 'cost':
                     t = threading.Thread(target=opt_cost, 
                                         args=(plants, conditions, results, pop_size, 
                                         n_gens))
-                                        # round((float(n_gens/n_runs))*run_count)))  # TESTING LINE. USED FOR SHOWING THE DIFFERENCE MADE BY HAVING A DIFFERENT NUMBER OF GENS
-                    run_count += 1
-                    threads.append(t)
+                                        # round((float(n_gens/n_runs))*run_count)))  # TESTING LINE. USED FOR SHOWING THE DIFFERENCE MADE BY HAVING A DIFFERENT NUMBER OF GENS            
+                case 'cost_water':
+                    t = threading.Thread(target=opt_cost_water_carbon, 
+                                        args=(plants, conditions, results, pop_size, n_gens))
+                    
+                case _: print('Optimization type not recognized.')
 
-                for i, t in enumerate(threads): 
-                    t.start()
-                    print(f'Thread {i+1}/{n_threads} in batch {b+1}/{n_batches} running...')  # lets you know what threads are running
+            run_count += 1
+            threads.append(t)
 
-                for t in threads: 
-                    t.join()
-                print(f'Batch {b+1} threads done in {round(time.time() - time_start, 2)}s.')
-            
-            # comment all above case and uncomment this to show generation graph (i think i can delete this :P)
-            # opt_cost(plants, conditions, results, pop_size, n_gens)
+        for i, t in enumerate(threads): 
+            t.start()
+            print(f'Thread {i+1}/{n_threads} in batch {b+1}/{n_batches} running...')  # lets you know what threads are running
 
-        case _: print('Optimization type not recognized.')
+        for t in threads: 
+            t.join()
+        print(f'Batch {b+1} threads done in {round(time.time() - time_start, 2)}s.')
+    
+    # comment all above case and uncomment this to show generation graph (i think i can delete this :P)
+    # opt_cost(plants, conditions, results, pop_size, n_gens)
     
     opt_runtime = time.time() - opt_start
     dialog = f'Optimization done in {round(opt_runtime, 2)}s.'  # variable for popup box
@@ -141,7 +145,7 @@ def opt_cost(plants: list[PowerPlant], conditions: list[float], result_list, pop
                 # xl being what it is is so that theres a smaller but still attainable chance 
                 #      to generate a negative number, which translates later to the plant just being off
                 # (min - max) just keeps the chance equal for all plants                
-                # TODO translate lower costs into a higher chance to have more power drawn?
+                # IGNORETODO translate lower costs into a higher chance to have more power drawn?
                 #      right now, they all have a 20% chance to be off. what if, like, the cheapest one had a 
                 #      2% chance, next cheapest a bit higher, all the way up to the most expensive having a 
                 #      ~90% chance to be off. you feelin me here (yes i am youre so cool) wow thanks me
@@ -154,7 +158,7 @@ def opt_cost(plants: list[PowerPlant], conditions: list[float], result_list, pop
                 # xl.append((p.min_output - p.max_output) / cdict_xl[p.plant_cost])
                 # xu.append((p.max_output - p.min_output) * cdict_xu[p.plant_cost])
 
-                # TODO add optimal solution manually w/ function?
+                # IGNORETODO add optimal solution manually w/ function?
 
             # number of constraints = number of plants (16), demand met (1), the tiers thing (1)
             num_constr = len(plants) + 1 + 1
@@ -187,7 +191,7 @@ def opt_cost(plants: list[PowerPlant], conditions: list[float], result_list, pop
             if demand_met < 0: demand_met = 0  # sets to 0 if the demand was successfully met
             constraint_violations.append(demand_met)
 
-            # TODO constraint violations pt. 4: add tiers of price (.07, .08, etc) and make it so its not 
+            # IGNORETODO constraint violations pt. 4: add tiers of price (.07, .08, etc) and make it so its not 
             #      acceptable if a more expensive tier is being used while a cheaper tier isnt full yet?
             # dawg im gonna be real i think this is gonna have to be a tomorrow thing. i CANNOT think rn
             if np.array(constraint_violations).sum() == 0:  # only do this if everything else is already fine
@@ -235,7 +239,144 @@ def opt_cost(plants: list[PowerPlant], conditions: list[float], result_list, pop
     result.X = normalize_x(result.X, [p.min_output for p in plants], [p.max_output for p in plants])
     # result.X = round_x(result.X)
 
-    # TODO round x to nearest 100 (or 10), then make sure constraints still arent violated and stuff
+    # IGNORETODO round x to nearest 100 (or 10), then make sure constraints still arent violated and stuff
+    # also have one that can not be rounded? like, after all rounding is done, add plants until a plant puts
+    # the total over where it needs to be, then only pull what's necessary from that plant
+    # i think that makes sense. its so late and i have so much caffeine in my bloodstream
+
+    # avoid nonetypes in the result array
+    # if sum(result.G) == 0:
+    #     result_list.append(result)
+    result_list.append(result)
+
+    # graph gens on x and F on y. ONLY WORKS WHEN RUNNING ON MAIN THREAD
+    # display_graph('gen_vs_res', history=result.history)
+
+
+# this should find the best balance between the water use, carbon emission, and cost
+# starting off from a copy paste of the opt-cost
+def opt_cost_water_carbon(plants: list[PowerPlant], conditions: list[float], result_list, population_size=10, num_gens=10):
+    # CLASSES !!!
+    class CostOptProblem(ElementwiseProblem):
+        def __init__(self, **kwargs):
+            # xl and xu (upper and lower bounds for x/cost) here
+            xl: list[int] = []
+            xu: list[int] = []
+
+            # gives me a sorted list of unique costs to use for xl/xu chance
+            costs = list(set([p.plant_cost for p in plants]))
+            costs.sort()
+
+            cdict_xl = {}
+            cdict_xu = {}
+
+            # this should make it so that lower costs have a higher chance of having their lower bound be 
+            # max and their upper bound be 
+            ind = len(costs)
+            for i, c in enumerate(costs): 
+                cdict_xl[c] = ind * (90 / len(costs))
+                ind -= 1
+                cdict_xu[c] = (i + 1) * (90 / len(costs))
+
+            for p in plants:  # finding min and max possible outputs for plants
+                # xl being what it is is so that theres a smaller but still attainable chance 
+                #      to generate a negative number, which translates later to the plant just being off
+                # (min - max) just keeps the chance equal for all plants                
+                # IGNORETODO translate lower costs into a higher chance to have more power drawn?
+                #      right now, they all have a 20% chance to be off. what if, like, the cheapest one had a 
+                #      2% chance, next cheapest a bit higher, all the way up to the most expensive having a 
+                #      ~90% chance to be off. you feelin me here (yes i am youre so cool) wow thanks me
+
+                # using 4 in place of everything to the right of the operators gives pretty damn 
+                # good answers already. just so you know.
+                xl.append((p.min_output - p.max_output) / 4)
+                xu.append((p.max_output - p.min_output) * 4)
+
+                # xl.append((p.min_output - p.max_output) / cdict_xl[p.plant_cost])
+                # xu.append((p.max_output - p.min_output) * cdict_xu[p.plant_cost])
+
+                # IGNORETODO add optimal solution manually w/ function?
+
+            # number of constraints = number of plants (16), demand met (1), the tiers thing (1)
+            num_constr = len(plants) + 1 + 1
+            super().__init__(n_var=len(plants), n_obj=1, xl=np.array(xl), xu=np.array(xu), n_constr=num_constr, vtype=int, **kwargs)
+
+        def _evaluate(self, x, out):
+            # reset tracking lists to empty
+            constraint_violations: list[bool] = []
+
+            power_demand = conditions[0]  # 13600 mW right now
+            plant_costs = [p.plant_cost for p in plants]
+
+            x = normalize_x(x, [p.min_output for p in plants], [p.max_output for p in plants])
+            # x = round_x(x)
+
+            for ind, mw in enumerate(x):
+                mi = plants[ind].min_output
+                ma = plants[ind].max_output
+
+                # its not graceful but it works
+                # ok so bad if:
+                #   1. less than min but not 0 OR greater than max
+                #   2. more than one less than max but not 0
+                if (mw < mi and mw != 0) or mw > ma:  # if EITHER between min and zero OR greater than max
+                    constraint_violations.append(1)
+                else: constraint_violations.append(0)
+                    
+            # constraint violations pt 3: was the demand met?
+            demand_met = power_demand - x.sum()
+            if demand_met < 0: demand_met = 0  # sets to 0 if the demand was successfully met
+            constraint_violations.append(demand_met)
+
+            # IGNORETODO constraint violations pt. 4: add tiers of price (.07, .08, etc) and make it so its not 
+            #      acceptable if a more expensive tier is being used while a cheaper tier isnt full yet?
+            # dawg im gonna be real i think this is gonna have to be a tomorrow thing. i CANNOT think rn
+            if np.array(constraint_violations).sum() == 0:  # only do this if everything else is already fine
+                constraint_violations.append(0)
+            else: constraint_violations.append(0)
+
+            # function ends here
+            out["F"] = round((x * 1000 * np.array(plant_costs)).sum(), 2)
+            out["G"] = constraint_violations # <= 0 (all values false) is ok, > 0 is not
+
+    class ProgressBarCallback(Callback):  # updates the progress bar every generation
+        def notify(self, alg):  # it won't let me not have an algorithm variable, even if its not used. lmao
+            # bar()
+            pass
+
+    prb = CostOptProblem()
+
+    alg = gen_alg(
+        pop_size=population_size,
+        sampling=IntegerRandomSampling(),
+        crossover=SBX(repair=RoundingRepair()),
+        selection=TournamentSelection(pressure=2, func_comp=binary_cv_tourney),
+        mutation=PM(prob=1, repair=RoundingRepair()),
+        eliminate_duplicates=True
+    )
+
+    # terminations okay for now
+    trm = get_termination(
+        'n_gen',
+        num_gens
+    )
+
+    # with progress_bar as bar:
+    result = minimize(  # go go gadget cheap electric
+        problem=prb,
+        algorithm=alg,
+        termination=trm,
+        # seed=int(time.time()),
+        verbose=False,
+        callback=ProgressBarCallback(),
+        save_history=True,  # can change if necessary, i just dont wanna waste resources im not even using lol
+        return_least_infeasible=True
+    )
+
+    result.X = normalize_x(result.X, [p.min_output for p in plants], [p.max_output for p in plants])
+    # result.X = round_x(result.X)
+
+    # IGNORETODO round x to nearest 100 (or 10), then make sure constraints still arent violated and stuff
     # also have one that can not be rounded? like, after all rounding is done, add plants until a plant puts
     # the total over where it needs to be, then only pull what's necessary from that plant
     # i think that makes sense. its so late and i have so much caffeine in my bloodstream
@@ -317,9 +458,11 @@ def round_x(unrounded_x):
 plants = get_test_plants()
 conditions = get_test_conditions()
 
-optimal_costs = optimize('cost', plants, conditions#, 
+# optimal_costs = optimize('cost', plants, conditions#, 
                         #  graph_best_res=True,
                         #  alert_when_done=True,
                         #  graph_gens='all'
-                         )
+                        #  )
+
+optimal_costs = optimize('cost', plants, conditions)
 
