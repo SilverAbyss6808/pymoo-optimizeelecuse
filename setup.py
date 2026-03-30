935c0ab32a0d8ff21f3a135b439e8b8ec824f695
l-baxter1 <l-baxter@onu.edu>
2026-03-30 15:10:02 -0400

    working on structuring for use as a package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='poeu-scripts',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Helper scripts for the electricity optimization game.',
+    author='Lila Baxter',
+)
